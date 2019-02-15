c980afdec3044134966fa0bbbbec48f08d6a8b3c
sandeep-r-m <srm4knowledge@gmail.com>
2019-02-15 22:22:19 +0530

    oms microservices | updated spring boot start pom version to 2.0.3.RELEASE

--- oms-order-service/src/main/java/com/sandeeprm/oms/orderservice/OmsOrderServiceApplication.java
+++ oms-order-service/src/main/java/com/sandeeprm/oms/orderservice/OmsOrderServiceApplication.java
@@ -3,21 +3,20 @@ package com.sandeeprm.oms.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.util.SocketUtils;
 
 @EnableEurekaClient
 @SpringBootApplication
 public class OmsOrderServiceApplication {
 
 	public static void main(String[] args) {
-		configure();
+		// configure();
 		SpringApplication.run(OmsOrderServiceApplication.class, args);
 	}
 
-	private static void configure() {
-		// Override server.port at runtime
-		int port = SocketUtils.findAvailableTcpPort();
-		System.setProperty("server.port", port + "");
-	}
+	/*
+	 * private static void configure() { // Override server.port at runtime int port
+	 * = SocketUtils.findAvailableTcpPort(); System.setProperty("server.port", port
+	 * + ""); }
+	 */
 
 }
